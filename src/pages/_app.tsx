404c4fbc2990dce08225ba9da9540a860584fbeb
neeleshj <neeleshjjoshi@gmail.com>
2022-04-28 17:24:15 +0100

    lol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,14 @@
 import { ChakraProvider, Flex } from '@chakra-ui/react';
-import { Fonts } from '../styles/fonts';
-import { PageTransition } from 'next-page-transitions';
-
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app';
+import { Fonts } from '../styles/fonts';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider>
       <Fonts />
       <Flex direction="column" minHeight="100vh" width="100%">
-        <Flex direction="column" flexGrow={1}>
-          <PageTransition timeout={300} classNames="page-transition">
-            <Component {...pageProps} key={router.route} />
-          </PageTransition>
-          <style jsx global>{`
-            .page-transition-enter {
-              opacity: 0;
-            }
-            .page-transition-enter-active {
-              opacity: 1;
-              transition: opacity 300ms;
-            }
-            .page-transition-exit {
-              opacity: 1;
-            }
-            .page-transition-exit-active {
-              opacity: 0;
-              transition: opacity 300ms;
-            }
-          `}</style>
-        </Flex>
+        <Component {...pageProps} key={router.route} />
       </Flex>
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,63 @@
-import { Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return <Heading>Hello</Heading>;
+  const endDate = Date.parse('10 June 2022 00:12:34 GMT');
+
+  const [countDown, setCountDown] = useState(endDate - new Date().getTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const value = endDate - new Date().getTime();
+      setCountDown(value);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [endDate]);
+
+  function formatMilliseconds(milliseconds, padStart) {
+    function pad(num) {
+      return `${num}`.padStart(2, '0');
+    }
+    const asSeconds = milliseconds / 1000;
+
+    let hours = undefined;
+    let minutes = Math.floor(asSeconds / 60);
+    const seconds = Math.floor(asSeconds % 60);
+
+    if (minutes > 59) {
+      hours = Math.floor(minutes / 60);
+      minutes %= 60;
+    }
+
+    return hours
+      ? `${padStart ? pad(hours) : hours}:${pad(minutes)}:${pad(seconds)}`
+      : `${padStart ? pad(minutes) : minutes}:${pad(seconds)}`;
+  }
+
+  return (
+    <Flex
+      w="full"
+      height={100}
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor="black"
+      flex={1}
+    >
+      <Box width={{ base: 'full', md: '400px' }} textAlign={{ base: 'center' }}>
+        <Heading
+          color="gold"
+          fontSize={{ base: 'xl', md: '2xl', lg: '5xl', xl: '8xl' }}
+        >
+          {formatMilliseconds(countDown, true)}:
+          {countDown
+            .toString()
+            .slice(
+              countDown.toString().length - 3,
+              countDown.toString().length
+            )}
+        </Heading>
+      </Box>
+    </Flex>
+  );
 }
