86508600d6a06915dd1b77857971fbc2e06c06b4
Chris Edson <christopher.edson@gmail.com>
2022-04-29 21:38:02 +0100

    Re-factor to include days in countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,32 @@
 import { Box, Flex, Heading } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { intervalToDuration } from 'date-fns';
 
-  const endDate = Date.parse('27 May 2022 09:10:32 GMT');
+const endDate = new Date(Date.parse('27 May 2022 09:10:32 GMT'));
 
-  const [countDown, setCountDown] = useState(endDate - new Date().getTime());
+const getIntervalToDuration = () =>
+  intervalToDuration({
+    start: new Date(),
+    end: endDate,
+  });
+
 export default function Home(): JSX.Element {
+  const [intervalToDur, setIntervalToDur] = useState(getIntervalToDuration());
+
+  const [countDown, setCountDown] = useState(
+    endDate.getTime() - new Date().getTime()
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const value = endDate - new Date().getTime();
+      setIntervalToDur(getIntervalToDuration());
+
+      const value = endDate.getTime() - new Date().getTime();
       setCountDown(value);
     }, 11);
 
     return () => clearInterval(interval);
-  }, [endDate]);
-
-  function formatMilliseconds(milliseconds: number, padStart: boolean) {
-    function pad(num: number) {
-      return `${num}`.padStart(2, '0');
-    }
-    const asSeconds = milliseconds / 1000;
-
-    let hours = undefined;
-    let minutes = Math.floor(asSeconds / 60);
-    const seconds = Math.floor(asSeconds % 60);
-
-    if (minutes > 59) {
-      hours = Math.floor(minutes / 60);
-      minutes %= 60;
-    }
-
-    return hours
-      ? `${padStart ? pad(hours) : hours}:${pad(minutes)}:${pad(seconds)}`
-      : `${padStart ? pad(minutes) : minutes}:${pad(seconds)}`;
-  }
+  }, []);
 
   return (
     <Flex
@@ -49,7 +42,7 @@ export default function Home(): JSX.Element {
           color="red"
           fontSize={{ base: '2xl', md: '5xl', lg: '8xl', xl: '16xl' }}
         >
-          {formatMilliseconds(countDown, true)}:
+          {`${intervalToDur.days}:${intervalToDur.hours}:${intervalToDur.minutes}:${intervalToDur.seconds}:`}
           {countDown
             .toString()
             .slice(
